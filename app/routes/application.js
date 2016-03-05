b4cba874d7f009f0ee3136220b149904011bfa7a
Robert Wagner <rwwagner@vt.edu>
2016-03-05 18:17:44 -0500

    Add flexi, setup link-tos, add routes

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,34 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  links: Ember.A([
-    {
-      href: '#0',
-      selected: true,
-      text: 'About'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Technologies'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Team'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Portfolio'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Contact'
+  model() {
+    return {
+      links: Ember.A([
+        {
+          linkTo: 'home',
+          selected: true,
+          text: 'Home'
+        },
+        {
+          linkTo: 'technologies',
+          selected: false,
+          text: 'Technologies'
+        },
+        {
+          linkTo: 'team',
+          selected: false,
+          text: 'Team'
+        },
+        {
+          linkTo: 'portfolio',
+          selected: false,
+          text: 'Portfolio'
+        },
+        {
+          linkTo: 'contact',
+          selected: false,
+          text: 'Contact'
+        }
+      ])
     }
-  ]),
-  setupController(controller){
-    controller.set('links', this.get('links'));
   }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,9 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var shim = require('flexi/lib/pod-templates-shim');
+
+shim(EmberApp);
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
