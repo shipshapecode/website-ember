b0f24055bfe38839f2003038c1c47e8818590421
Robert Wagner <rwwagner@vt.edu>
2016-03-11 08:45:23 -0500

    Switch to greensock

--- app/components/hero-block/component.js
+++ app/components/hero-block/component.js
@@ -1,15 +1,28 @@
+/* global TimelineLite */
+
 import Ember from 'ember';
 import layout from './template';
-import Vivus from 'vivus';
+import TweenLite from 'tweenlite';
+
 
 export default Ember.Component.extend({
   layout,
   didInsertElement(){
-    new Vivus('svg-logo', {
-      duration: 100,
-      file: 'img/ShipShapeHeader.svg',
-      start: 'autostart',
-      type: 'oneByOne'
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      const paths = this.$('#Layer_7 path');
+
+      paths.each(function(i, e) {
+        e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
+      });
+
+      const tl = new TimelineLite();
+
+      tl.add([
+        TweenLite.to(paths.eq(0), 1, {strokeDashoffset: 0, delay: 0.5}),
+        TweenLite.to(paths.eq(1), 1, {strokeDashoffset: 0, delay: 0.5}),
+        TweenLite.to(paths.eq(2), 1, {strokeDashoffset: 0, delay: 0.5}),
+        TweenLite.to(paths.eq(3), 1, {strokeDashoffset: 0, delay: 0.5})
+      ]);
     });
   }
 });
