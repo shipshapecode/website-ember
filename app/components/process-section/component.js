aabfd3bdd7094b3e57d5f16ab7662383cc0488b0
Robert Wagner <rwwagner@vt.edu>
2016-03-12 18:14:43 -0500

    Add back virus, change main to screen

--- app/components/process-section/component.js
+++ app/components/process-section/component.js
@@ -1,13 +1,19 @@
-import Ember from 'ember';
+/* global SVGInjector */
 
-export default Ember.Component.extend({
+import Ember from 'ember';
+import InViewportMixin from 'ember-in-viewport';
+import Vivus from 'vivus';
+
+export default Ember.Component.extend(InViewportMixin, {
   didInsertElement(){
     // Elements to inject
     const mySVGsToInject = document.querySelectorAll('img.svg-icon');
 
     // Trigger the injection
     new SVGInjector(mySVGsToInject, {}, () => {
+      new Vivus('design-svg', {duration: 100, type: 'oneByOne'}, null);
+      new Vivus('build-svg', {duration: 100, type: 'oneByOne'}, null);
+      new Vivus('ship-svg', {duration: 100, type: 'oneByOne'}, null);
     });
-
   }
 });
