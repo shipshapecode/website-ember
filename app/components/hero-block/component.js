32aa4dbff0915afa2c960d8a3354478e74516c81
Robert Wagner <rwwagner@vt.edu>
2016-03-12 14:34:49 -0500

    Animation is acceptable

--- app/components/hero-block/component.js
+++ app/components/hero-block/component.js
@@ -24,14 +24,11 @@ export default Ember.Component.extend({
       const tl = new TimelineLite();
 
       tl.add([
-        TweenLite.to(paths.eq(0), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.to(paths.eq(1), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.to(paths.eq(2), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.to(paths.eq(3), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.to(paths.eq(4), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.to(paths.eq(5), 2, {strokeDashoffset: 0, delay: 0.5}),
-        TweenLite.from('#Boat', 1, {x: 200, delay: 1.7}),
-        TweenLite.from('#Dot', 1, {y: -1000, delay: 0.5})
+        TweenLite.to(paths.eq(0), 2, {strokeDashoffset: 0, delay: 1.5}),
+        TweenLite.to(paths.eq(1), 2, {strokeDashoffset: 0, delay: 1.5}),
+        TweenLite.to(paths.eq(2), 1, {strokeDashoffset: 0, delay: 0.5}),
+        TweenLite.from('#Boat', 1, {x: 200, delay: 1.5}),
+        TweenLite.from('#Dot', 1, {y: -1000, delay: 1.0})
       ]);
     });
 
