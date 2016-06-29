b5e2c75d101703acf102d0052ab19eab0dff25c0
Robert Wagner <rwwagner@vt.edu>
2016-06-28 21:11:18 -0400

    Switch title order

--- app/instance-initializers/after-meta.js
+++ app/instance-initializers/after-meta.js
@@ -2,7 +2,7 @@ export function initialize(appInstance) {
   const meta = appInstance.lookup('service:meta');
   meta.reopen({
     title: function (value) {
-      return value + ' - Ship Shape';
+      return 'Ship Shape - ' + value;
     }
   });
 }
