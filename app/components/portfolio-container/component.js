8517419d7e730afa972a57c61a5baa35a6f5aa81
Robert Wagner <rwwagner@vt.edu>
2016-08-09 19:37:38 -0400

    Add sponsored oss section, add ember-a11y

--- app/components/portfolio-container/component.js
+++ app/components/portfolio-container/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-  tagName: 'div',
+  tagName: 'page',
   classNameBindings: [':portfolio-container'],
   projectSorting: ['stars:desc'],
   projects: computed.sort('model', 'projectSorting')
