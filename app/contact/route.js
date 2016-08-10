aebb9c11b5afed25b603336b8706d51c7d14c989
Robert Wagner <rwwagner@vt.edu>
2016-08-10 19:54:49 -0400

    Add project type radio buttons

--- app/contact/route.js
+++ app/contact/route.js
@@ -3,6 +3,27 @@ const { inject: { service }, Route } = Ember;
 
 export default Route.extend({
   headData: service(),
+  model() {
+    return {
+      projectTypes: [
+        {
+          fieldName: 'oss',
+          name: 'project-type',
+          text: 'OSS'
+        },
+        {
+          fieldName: 'training',
+          name: 'project-type',
+          text: 'Ember Training'
+        },
+        {
+          fieldName: 'dev',
+          name: 'project-type',
+          text: 'Ember Development'
+        }
+      ]
+    };
+  },
   afterModel() {
     return this.get('headData').setProperties({
       title: 'Ship Shape - Contact',
