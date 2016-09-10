d60aa5ce2bea52b0be3c776cc26ce533e0e71cee
Robert Wagner <rwwagner@vt.edu>
2016-09-10 10:33:16 -0400

    Add empty folding panels

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -3,6 +3,20 @@ const { inject: { service }, Route } = Ember;
 
 export default Route.extend({
   headData: service(),
+  model() {
+    return [
+      {
+        heading: 'Scout',
+        imageURL: 'img/scout-logo.png',
+        panelHeading: 'Scout'
+      },
+      {
+        heading: 'Williamson Wagner',
+        imageURL: 'img/WW.png',
+        panelHeading: 'Williamson Wagner'
+      }
+    ];
+  },
   afterModel() {
     return this.get('headData').setProperties({
       title: 'Ship Shape - Ember Consulting',
