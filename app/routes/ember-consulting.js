c96e0db29f8e1ef4a5bb980f7f87f40131fefc4a
Robert Wagner <rwwagner@vt.edu>
2016-09-14 20:49:32 -0400

    Add some panelInfo

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-const { inject: { service }, Route } = Ember;
+const { inject: { service }, Route, String: { htmlSafe } } = Ember;
 
 export default Route.extend({
   headData: service(),
@@ -8,12 +8,37 @@ export default Route.extend({
       {
         heading: 'Scout',
         imageURL: 'img/scout-logo.png',
-        panelHeading: 'Scout'
+        panelHeading: 'Scout',
+        panelSubheading: 'Code audit, app feature development',
+        panelInfo: htmlSafe(
+          `<p>
+            We did several months of work with Scout,
+            starting with updating to the latest versions of
+            Ember and Ember CLI, adding Ember Suave, and adhering
+            to the new set of ESLint rules.
+          </p>
+
+          <p>
+            We worked extensively on the appointment scheduling
+            aspects of the app, creating full screen overlays,
+            using Ember Flatpickr, Ember Paper and Ember Power Select,
+            to allow a customer or staff member to create or edit an
+            appointment.
+          </p>`
+        )
       },
       {
         heading: 'Williamson Wagner',
         imageURL: 'img/WW.png',
-        panelHeading: 'Williamson Wagner'
+        panelHeading: 'Williamson Wagner',
+        panelSubheading: 'Logo design, web design, and website',
+        panelInfo: htmlSafe(
+          `<p>
+            We worked on every facet of Williamson Wagner's brand.
+            From logo designs, to shirt designs, to web design and
+            full development of williamsonwagner.com.
+          </p>`
+        )
       }
     ];
   },
