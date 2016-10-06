67946f5d2b8e481823ffb044833d88b89172b113
Robert Wagner <rwwagner@vt.edu>
2016-10-05 20:24:31 -0400

    Add back firebase

--- app/routes/open-source.js
+++ app/routes/open-source.js
@@ -4,56 +4,7 @@ const { inject: { service }, Route } = Ember;
 export default Route.extend({
   headData: service(),
   model() {
-    // return this.store.findAll('github-repo');
-
-    // Use static data for now, since EmberFire gives Fastboot Errors
-    return [
-      {
-        'name': 'Ember 3D Nav',
-        'repo': 'ember-3d-nav',
-        'stars': 10
-      },
-      {
-        'name': 'Ember Drop',
-        'repo': 'ember-drop',
-        'stars': 4
-      },
-      {
-        'name': 'Ember Flatpickr',
-        'repo': 'ember-flatpickr',
-        'stars': 16
-      },
-      {
-        'name': 'Ember Math Helpers',
-        'repo': 'ember-math-helpers',
-        'stars': 17
-      },
-      {
-        'name': 'Ember Newton Cradle Loader',
-        'repo': 'ember-newton-cradle-loader',
-        'stars': 0
-      },
-      {
-        'name': 'Ember Shepherd',
-        'repo': 'ember-shepherd',
-        'stars': 66
-      },
-      {
-        'name': 'Ember Typewriter',
-        'repo': 'ember-typewriter',
-        'stars': 0
-      },
-      {
-        'name': 'Ember Vivus',
-        'repo': 'ember-vivus',
-        'stars': 0
-      },
-      {
-        'name': 'Ember X-Editable',
-        'repo': 'ember-x-editable',
-        'stars': 13
-      }
-    ];
+    return this.store.findAll('github-repo');
   },
   afterModel() {
     return this.get('headData').setProperties({
