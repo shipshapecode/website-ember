ca3254a8bd48c285975c78ffe3a10a6b4093f8ea
Robert Wagner <rwwagner@vt.edu>
2016-10-18 21:44:22 -0400

    Start adding some actual tests

--- tests/unit/controllers/contact-test.js
+++ tests/unit/controllers/contact-test.js
@@ -4,8 +4,14 @@ moduleFor('controller:contact', 'Unit | Controller | contact', {
   needs: ['service:fastboot', 'service:metrics', 'service:router-scroll']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('sendDisabled calculates correctly', function(assert) {
   let controller = this.subject();
-  assert.ok(controller);
+
+  controller.set('model', { validations: { isValid: false } });
+
+  assert.equal(controller.get('sendDisabled'), true, 'Disabled inititally');
+
+  controller.set('model.validations.isValid', true);
+
+  assert.equal(controller.get('sendDisabled'), false, 'Enabled when valid');
 });
--- tests/unit/models/contact-test.js
+++ tests/unit/models/contact-test.js
@@ -1,12 +1,22 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
+const { run } = Ember;
 
 moduleForModel('contact', 'Unit | Model | contact', {
   // Specify the other units that are required for this test.
   needs: ['validator:format', 'validator:presence']
 });
 
-test('it exists', function(assert) {
+test('model validations are correct', function(assert) {
   let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+
+  assert.equal(model.get('validations.isValid'), false, 'Model is invalid');
+
+  run(() =>{
+    model.set('description', 'Test description');
+    model.set('email', 'foo@bar.com');
+    model.set('name', 'John Smith');
+
+    assert.equal(model.get('validations.isValid'), true, 'Model is valid');
+  });
 });
