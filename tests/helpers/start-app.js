18ce9ec11b1b5d44e66ea2fe013dd2d241c50ec5
Robert Wagner <rwwagner90@gmail.com>
2016-12-26 01:18:46 -0500

    Update CLI

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-const { assign, run } = Ember;
+const { merge, run } = Ember;
 
 export default function startApp(attrs) {
   let application;
 
-  // use defaults, but you can override
-  const attributes = assign({}, config.APP, attrs);
+  let attributes = merge({}, config.APP);
+  attributes = merge(attributes, attrs); // use defaults, but you can override;
 
   run(() => {
     application = Application.create(attributes);
