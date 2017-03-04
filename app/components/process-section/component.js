707d260eab930bef030a43c2a1f52de189d5c73f
Robert Wagner <rwwagner90@gmail.com>
2017-03-03 21:45:26 -0500

    Rework icons and animations

--- app/components/process-section/component.js
+++ app/components/process-section/component.js
@@ -4,8 +4,14 @@ import Vivus from 'vivus';
 
 export default Component.extend(InViewportMixin, {
   didInsertElement() {
-    new Vivus('design-svg', { duration: 100, type: 'oneByOne' }, null);
-    new Vivus('build-svg', { duration: 100, type: 'oneByOne' }, null);
-    new Vivus('ship-svg', { duration: 100, type: 'oneByOne' }, null);
+    new Vivus('design-svg', { duration: 100, type: 'delayed' }, function(obj) {
+      obj.el.classList.add('finished');
+    });
+    new Vivus('build-svg', { duration: 100, type: 'delayed' }, function(obj) {
+      obj.el.classList.add('finished');
+    });
+    new Vivus('ship-svg', { duration: 100, type: 'delayed' }, function(obj) {
+      obj.el.classList.add('finished');
+    });
   }
 });
