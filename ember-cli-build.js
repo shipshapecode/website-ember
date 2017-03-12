31afe5529e749e518a2ba8ccce4d67641705c4a3
Robert Wagner <rwwagner90@gmail.com>
2017-03-12 15:00:20 -0400

    Use webp images

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -63,5 +63,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/modernizr.min.js');
+
   return app.toTree();
 };
