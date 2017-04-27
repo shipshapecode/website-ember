1ae7347eeb0af2a1d72520393c9f3974f8744606
Robert Wagner <rwwagner90@gmail.com>
2017-04-27 15:33:50 -0400

    Use targets stuff

--- config/targets.js
+++ config/targets.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   browsers: [
-    'ie 9',
     'last 1 Chrome versions',
     'last 1 Firefox versions',
-    'last 1 Safari versions'
+    'last 1 Safari versions',
+    'last 1 Edge versions'
   ]
 };
