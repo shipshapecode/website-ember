25c9576a1058c89c5d8a007ae9b867dffe06cb53
Robert Wagner <rwwagner90@gmail.com>
2017-07-26 17:40:57 -0400

    Add a basic square with minimal styles and info for greenlight.guru

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -59,6 +59,19 @@ export default Route.extend({
             full development of williamsonwagner.com.
           </p>`
         )
+      },
+      {
+        heading: 'greenlight.guru',
+        imageURL: 'img/clients/greenlight-logo.png',
+        panelHeading: 'greenlight.guru',
+        panelSubheading: '1.10 - 1.13 update and deprecation fixes, grunt to ember-cli',
+        panelInfo: htmlSafe(
+          `<p>
+            We first updated the greenlight.guru app to Ember 1.12, fixing deprecations as we went.
+            After 1.12 was complete, we undertook the task of migrating a grunt based build to ember-cli.
+            We finally updated the app to Ember 1.13 and fixed all the deprecations.
+          </p>`
+        )
       }
     ];
   },
