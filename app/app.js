11003f64d2ac8c75098253257733395cee9c60a2
Robert Wagner <rwwagner90@gmail.com>
2017-07-30 11:42:24 -0400

    Fix fastboot issues

--- app/app.js
+++ app/app.js
@@ -1,10 +1,7 @@
 import Application from '@ember/application';
-import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
