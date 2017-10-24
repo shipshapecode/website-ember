d932d9ec608a6ff1de74e896cff0e444c92d5bd1
Robert Wagner <rwwagner90@gmail.com>
2017-10-23 20:43:38 -0400

    Add details for MT

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -11,18 +11,31 @@ export default Route.extend({
         heading: 'Mariana Tek',
         imageURL: 'img/clients/mariana-tek.svg',
         panelHeading: 'Mariana Tek',
-        panelSubheading: 'MVP app development and staff training',
+        panelSubheading: 'Staff augmentation for initial release',
         panelInfo: htmlSafe(
           `<p>
-            We worked on the Mentrs MVP from the ground up,
-            starting with running 'ember new', and adding all the addons, 
-            ESLint plugins, template lintings, and doing all the initial 
-            work required to create an Ember app, rooted in the latest Ember standards.
+            We worked hand in hand with the Mariana Tek dev team to develop several required features for their first
+            major client release. This included an initial scan of the codebase, the addition of several ESLint rules from
+            eslint-plugin-ember, and updates of various dependencies. We enforced best practices at every turn, and ensured the
+            code looked very consistent, no matter who wrote it.
           </p>
 
           <p>
-            Features we implemented included everything from login and sign up, 
-            to class creation, and Stripe integration for paying to enroll in classes.
+            We worked heavily on implementing the promotions page, which allowed clients to choose items to put on sale,
+            add requirements for the sale to become active, such as having multiple items in their cart or a certain cart value,
+            and allowing custom product variants to be used.
+          </p>
+          
+          <p>
+            We also did a lot of work on the time clock features. This included adding a clock in/out modal that would allow
+            employees to clock in and out and add notes about their shifts, as well as a configuration page that showed a table
+            of all the most recent shifts, utilizing ember-light-table, and allowed sorting by shift time and employee name.
+            Admins were also able to manually edit or add shifts, if something needed to be tweaked from employee shifts.
+          </p>
+          
+          <p>
+            The rest of our work focused mainly on the permissions service and ensuring users could only access the parts
+            of the app they were allowed to, based on their roles.
           </p>`
         )
       },
