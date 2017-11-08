60b64a0db087aaef2c07a19b7338fa25db9a5ce7
Robert Wagner <rwwagner90@gmail.com>
2017-11-08 18:21:31 -0500

    Attempt to use fastboot-app-server

--- serve.js
+++ serve.js
@@ -3,16 +3,16 @@ const FastBootAppServer = require('fastboot-app-server');
 
 const server = new FastBootAppServer({
   distPath: 'dist',
-  gzip: true,
-  beforeMiddleware(app) {
-    app.use((request, response, next) => {
-      if (request.headers['x-forwarded-proto'] === 'https') {
-        return next();
-      } else {
-        return response.redirect(301, `https://${request.hostname}${request.url}`);
-      }
-    });
-  }
+  gzip: true
+  // beforeMiddleware(app) {
+  //   app.use((request, response, next) => {
+  //     if (request.headers['x-forwarded-proto'] === 'https') {
+  //       return next();
+  //     } else {
+  //       return response.redirect(301, `https://${request.hostname}${request.url}`);
+  //     }
+  //   });
+  // }
 });
 
 server.start();
