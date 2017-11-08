16c6bf3f9e7372b53c3347b210f3b8c8b8381abe
Robert Wagner <rwwagner90@gmail.com>
2017-11-08 12:54:58 -0500

    Remove mixin and override ajaxOptions in adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,13 @@
-import AdapterFetch from '../mixins/adapter-fetch';
+import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import ENV from '../config/environment';
 
 export default JSONAPIAdapter.extend(AdapterFetch, {
-  host: ENV.host
+  host: ENV.host,
+
+  ajaxOptions() {
+    const options = this._super(...arguments);
+    options.headers['content-type'] = 'application/vnd.api+json';
+    return options;
+  }
 });
