518f23da8d414127172059ea5687a6534e158301
Robert Wagner <rwwagner90@gmail.com>
2018-01-31 11:11:28 -0500

    Remove router-scroll

--- app/initializers/trailing-history.js
+++ app/initializers/trailing-history.js
@@ -1,11 +1,4 @@
 import HistoryLocation from '@ember/routing/history-location';
-import { set, get } from '@ember/object';
-
-const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-  const r = Math.random() * 16 | 0;
-  const v = c === 'x' ? r : r & 3 | 8;
-  return v.toString(16);
-});
 
 let trailingHistory = HistoryLocation.extend({
   formatURL() {
@@ -16,17 +9,7 @@ let trailingHistory = HistoryLocation.extend({
     } else {
       return url.replace(/\/?$/, '/');
     }
-  },
-  pushState(path) {
-    const state = { path, uuid: uuid() };
-    get(this, 'history').pushState(state, null, path);
-    set(this, 'previousURL', this.getURL());
-  },
-  replaceState(path) {
-    const state = { path, uuid: uuid() };
-    get(this, 'history').replaceState(state, null, path);
-    set(this, 'previousURL', this.getURL());
-  },
+  }
 });
 
 export default {
