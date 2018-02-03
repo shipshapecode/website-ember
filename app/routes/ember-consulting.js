75ff4859db277c67a4d17a4117bc6335e2ee39cc
Robert Wagner <rwwagner90@gmail.com>
2018-02-02 22:33:19 -0500

    Tweak consulting wording

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -125,7 +125,7 @@ export default Route.extend({
       title: 'Ember.js Consulting and Training - Ship Shape',
       description: 'Ember consulting is our specialty at Ship Shape. We have worked on several ambitious, ' +
       'full scale Ember apps for both large and small clients. We put a huge focus on leveling up your team\'s ' +
-      'Ember skills, mentoring, and pairing while also churning out bulletproof features for your Ember app.',
+      'Ember skills, while also churning out bulletproof features for your Ember app.',
       type: 'website',
       url: 'https://shipshape.io/ember-consulting'
     });
