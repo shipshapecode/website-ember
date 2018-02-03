b9fe78f04dc77aaa3f6c17eba9be84466b3f5150
Robert Wagner <rwwagner90@gmail.com>
2018-02-02 22:31:31 -0500

    Tweak wording

--- app/routes/home.js
+++ app/routes/home.js
@@ -17,7 +17,7 @@ export default Route.extend({
       description:
       'Ship Shape is a Washington D.C. based software consultancy specializing in all things Ember. ' +
       'We leverage Ember.js, and all the latest Ember addons and technologies, to create truly ambitious, ' +
-      'state of the art applications that are future-proof, bulletproof, and easily maintainable.',
+      'state of the art applications that are future-proof and easily maintainable.',
       type: 'website',
       url: 'https://shipshape.io/'
     });
