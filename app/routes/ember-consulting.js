1305e0bc8567d4a070066c98af0653e6ef382097
Robert Wagner <rwwagner90@gmail.com>
2018-02-02 21:57:23 -0500

    Ember consulting description

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -123,8 +123,9 @@ export default Route.extend({
   afterModel() {
     return setProperties(get(this, 'headData'), {
       title: 'Ember.js Consulting and Training - Ship Shape',
-      description: 'Ember consulting is our specialty at Ship Shape. We have worked on several ambitious, '
-      + 'full scale Ember apps. Contact us for Ember consulting work today!',
+      description: 'Ember consulting is our specialty at Ship Shape. We have worked on several ambitious, ' +
+      'full scale Ember apps for both large and small clients. We put a huge focus on leveling up your team\'s ' +
+      'Ember skills, mentoring, and pairing while also churning out bulletproof features for your Ember app.',
       type: 'website',
       url: 'https://shipshape.io/ember-consulting'
     });
