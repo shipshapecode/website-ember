cab4ae8516ddcc2959ed4e689b136ac8405f711f
Robert Wagner <rwwagner90@gmail.com>
2018-02-02 21:48:32 -0500

    Add to home description

--- app/routes/home.js
+++ app/routes/home.js
@@ -14,8 +14,10 @@ export default Route.extend({
   afterModel() {
     return setProperties(get(this, 'headData'), {
       title: 'Ship Shape - Ember.js Code That Won\'t Sink',
-      description: 'Ship Shape is an Ember consulting agency. We leverage Ember.js, and all the '
-      + 'latest Ember addons and technologies, to create truly ambitious applications.',
+      description:
+      'Ship Shape is a Washington D.C. based software consultancy specializing in all things Ember. ' +
+      'We leverage Ember.js, and all the latest Ember addons and technologies, to create truly ambitious, ' +
+      'state of the art applications that are future-proof, bulletproof, and easily maintainable.',
       type: 'website',
       url: 'https://shipshape.io/'
     });
