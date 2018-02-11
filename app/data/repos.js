eea2c787e31fea9bb2b52f85468d055fbcf2615c
Robert Wagner <rwwagner90@gmail.com>
2018-02-11 12:41:15 -0500

    Add descriptions to repos sample data

--- app/data/repos.js
+++ app/data/repos.js
@@ -1,50 +1,72 @@
 export default [
   {
+    description: 'An Ember addon for the site tour library Shepherd',
     name: 'ember-shepherd',
-    stars: 103
+    stars: 121
   },
   {
+    description: 'An Ember CLI addon for versioned release management',
+    name: 'ember-cli-release',
+    stars: 89
+  },
+  {
+    description: 'Ember HTMLBars helpers for basic arithmetic',
     name: 'ember-math-helpers',
-    stars: 53
+    stars: 69
   },
   {
+    description: 'An Ember addon that wraps the Flatpickr date picker',
     name: 'ember-flatpickr',
-    stars: 36
+    stars: 62
   },
   {
+    description: 'X-editable like library for Ember',
     name: 'ember-x-editable',
-    stars: 15
+    stars: 18
   },
   {
+    description: 'A 3d rotating top navigation bar for Ember apps',
     name: 'ember-3d-nav',
-    stars: 13
+    stars: 18
   },
   {
+    description: 'An Ember addon that wraps Drop.js',
     name: 'ember-drop',
     stars: 8
   },
   {
+    description: 'The code for shipshape.io',
     name: 'shipshape.io',
     stars: 6
   },
   {
+    description: 'Ember addon for showing a newton cradle loading animation',
     name: 'ember-newton-cradle-loader',
     stars: 3
   },
   {
+    description: 'An Ember wrapper for Typed.js',
     name: 'ember-typewriter',
     stars: 2
   },
   {
+    description: 'An Instagram feed for your Ember app',
     name: 'ember-instafeed',
     stars: 1
   },
   {
+    description: 'An Ember addon for 3d folding panels',
     name: 'ember-3d-folding-panel',
     stars: 1
   },
   {
+    description: 'Ember addon to include headroom.js as an ES2015 module in your app',
     name: 'ember-headroom',
     stars: 0
+  },
+  {
+    description: 'An Ember Service Worker plugin that caches the index.html files for each prember route',
+    name: 'ember-service-worker-prember',
+    stars: 4
   }
 ];
