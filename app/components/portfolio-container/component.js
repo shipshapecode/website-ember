392c809ebde826f0c25c2bbfe9fc7be3f72d6e48
Robert Wagner <rwwagner90@gmail.com>
2018-02-13 10:19:08 -0500

    Finish fixing styles

--- app/components/portfolio-container/component.js
+++ app/components/portfolio-container/component.js
@@ -3,7 +3,7 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: 'page',
-  classNameBindings: [':portfolio-container'],
+  classNames: ['portfolio-container'],
   projects: computed.sort('model', 'projectSorting'),
   init() {
     this._super(...arguments);
