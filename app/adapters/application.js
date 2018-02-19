3f345106b098fc408dac17d8a8d346cee761fd84
Robert Wagner <rwwagner90@gmail.com>
2018-02-19 08:16:19 -0500

    Remove ember-fetch for 3.1 compatibility

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,7 @@
-import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import ENV from '../config/environment';
 
-export default JSONAPIAdapter.extend(AdapterFetch, {
+export default JSONAPIAdapter.extend({
   host: ENV.host,
 
   ajaxOptions() {
