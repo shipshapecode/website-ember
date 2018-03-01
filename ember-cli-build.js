db8b1489f1e99d6671368c7ebcd5fd929654dba6
Robert Wagner <rwwagner90@gmail.com>
2018-02-28 21:57:08 -0500

    Bump deps, bust cache

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,7 +15,7 @@ module.exports = function(defaults) {
         'img/**/*',
         'svgs/**/*'
       ],
-      version: '24'
+      version: '25'
     },
     'ember-service-worker': {
       registrationStrategy: 'inline',
@@ -27,7 +27,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '24'
+      version: '25'
     },
     emberCliConcat: {
       js: {
