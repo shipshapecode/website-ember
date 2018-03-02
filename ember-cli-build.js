da3f875acbc2948b9ac825b365b89be8e2a06fd8
Robert Wagner <rwwagner90@gmail.com>
2018-03-02 09:20:04 -0500

    Inline font, update deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,7 +15,7 @@ module.exports = function(defaults) {
         'img/**/*',
         'svgs/**/*'
       ],
-      version: '31'
+      version: '32'
     },
     'ember-service-worker': {
       registrationStrategy: 'inline',
@@ -27,7 +27,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '31'
+      version: '32'
     },
     emberCliConcat: {
       js: {
