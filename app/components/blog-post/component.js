8e2b8d6ddafa4c6edc303227ee39b8b9cf2e6b6a
Robert Wagner <rwwagner90@gmail.com>
2018-03-20 03:35:22 -0400

    Go back to previous blog post format

--- app/components/blog-post/component.js
+++ app/components/blog-post/component.js
@@ -1,13 +1,29 @@
-import { CompatComponent as Component } from 'ember-glimmer-component';
+import Component from '@ember/component';
 import { alias } from '@ember/object/computed';
 
 export default Component.extend({
-  tagName: '',
+  classNameBindings: ['slug'],
+  tagName: 'article',
 
   author: alias('post.attributes.author'),
   authorId: alias('post.attributes.authorId'),
   content: alias('post.content'),
   date: alias('post.attributes.date'),
   slug: alias('post.attributes.slug'),
-  title: alias('post.attributes.title')
+  title: alias('post.attributes.title'),
+
+  didRender(){
+    this._super(...arguments);
+
+    let nodeList = this.element.querySelectorAll('pre:not(.no-line-numbers) > code');
+
+    if (nodeList) {
+      // console.log(nodeList);
+      nodeList.forEach((code) => {
+        code.parentNode.classList.add('line-numbers');
+      });
+    }
+
+    Prism.highlightAll();
+  }
 });
