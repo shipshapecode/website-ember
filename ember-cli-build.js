a3917f65195ddef7781d793b9ed17f3ec273701f
Robert Wagner <rwwagner90@gmail.com>
2018-04-28 22:13:02 -0400

    Bust cache

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,7 +18,7 @@ module.exports = function(defaults) {
         'fonts/**/*',
         'img/**/*'
       ],
-      version: '44'
+      version: '45'
     },
     'esw-cache-fallback': {
       patterns: [
@@ -26,7 +26,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '44'
+      version: '45'
     },
     emberCliConcat: {
       js: {
