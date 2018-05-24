af4985a3cc739cee90cb63b9a11d86e2920b9077
Chris Manson <chris@stonecircle.io>
2018-05-24 16:36:51 +0100

    demoing the attribute free ember-meta

---
+++ app/services/head-data.js
@@ -0,0 +1,9 @@
+import HeadData from 'ember-meta/services/head-data';
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
+
+export default HeadData.extend({
+  currentRouteModel: computed('routeName', function() {
+    return getOwner(this).lookup(`route:${this.get('routeName')}`).get('currentModel.attributes');
+  }),
+});
