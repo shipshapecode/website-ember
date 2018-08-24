0401463a0d195d0fb595519e3832270cecc48691
Robert Wagner <rwwagner90@gmail.com>
2018-08-24 09:22:16 -0400

    Bump deps

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,9 +1,10 @@
-/* jshint node:true */
 'use strict';
 
 module.exports = {
   extends: 'recommended',
   rules: {
+    'attribute-indentation': false,
+    'no-unnecessary-concat': false,
     'style-concatenation': false
   }
 };
