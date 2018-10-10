bd1fa17f4052eaaa5a811c817cf024e042446ad9
Robert Wagner <rwwagner90@gmail.com>
2018-10-09 22:40:51 -0400

    Remove ember-native-dom-helpers

--- tests/integration/components/scroll-suggest/component-test.js
+++ tests/integration/components/scroll-suggest/component-test.js
@@ -1,7 +1,6 @@
-import { find } from 'ember-native-dom-helpers';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | scroll suggest', function(hooks) {
