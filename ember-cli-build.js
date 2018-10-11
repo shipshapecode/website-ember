ed8b0904022cc0a2d87191b45882175c29517a47
Robert Wagner <rwwagner90@gmail.com>
2018-10-11 11:49:06 -0400

    Update ember-3d-nav

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -33,7 +33,7 @@ module.exports = function(defaults) {
         'fonts/**/*',
         'img/**/*'
       ],
-      version: '58'
+      version: '59'
     },
     'ember-cli-markdown-to-json': [
       {
@@ -80,7 +80,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '58'
+      version: '59'
     },
     emberCliConcat: {
       js: {
