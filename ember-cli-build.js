3f7aae724ee4d2d76f77498f148faa4adf94669f
Robert Wagner <rwwagner90@gmail.com>
2018-10-14 12:50:10 -0400

    Fix footer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -33,7 +33,7 @@ module.exports = function(defaults) {
         'fonts/**/*',
         'img/**/*'
       ],
-      version: '64'
+      version: '65'
     },
     'ember-cli-markdown-to-json': [
       {
@@ -80,7 +80,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '64'
+      version: '65'
     },
     emberCliConcat: {
       js: {
