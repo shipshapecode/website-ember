937e24944f749a304a0c710f0204edabe27801ad
Robert Wagner <rwwagner90@gmail.com>
2018-10-14 09:23:03 -0400

    ember-cli 3.5 beta

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -33,7 +33,7 @@ module.exports = function(defaults) {
         'fonts/**/*',
         'img/**/*'
       ],
-      version: '62'
+      version: '63'
     },
     'ember-cli-markdown-to-json': [
       {
@@ -80,7 +80,7 @@ module.exports = function(defaults) {
       ]
     },
     'esw-prember': {
-      version: '62'
+      version: '63'
     },
     emberCliConcat: {
       js: {
