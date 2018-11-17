1897f5512f01982a4f734167e83185afde2ec2b5
Robert Wagner <rwwagner90@gmail.com>
2018-11-17 11:07:22 -0500

    Fix author route

--- app/routes/blog/author.js
+++ app/routes/blog/author.js
@@ -1,29 +1,26 @@
-// import { A } from '@ember/array';
 import Route from '@ember/routing/route';
-// import RSVP from 'rsvp';
-// import { setProperties } from '@ember/object';
-// import { inject as service } from '@ember/service';
+import { setProperties } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  // headData: service(),
-  // markdownResolver: service(),
-  //
-  // model({ author }) {
-  //   return this.markdownResolver.tree('blog').then((tree) => {
-  //     return new RSVP.Promise((resolve) => {
-  //       const authorsPosts = A(tree.files.filterBy('attributes.authorId', author));
-  //       resolve(authorsPosts.sortBy('attributes.date').reverse());
-  //     });
-  //   });
-  // },
-  //
-  // afterModel(model) {
-  //   const author = model.get('firstObject.attributes.authorId');
-  //   return setProperties(this.headData, {
-  //     title: `${author} - Blog - Ship Shape`,
-  //     description: 'Ramblings about Ember.js, JavaScript, life, liberty, and the pursuit of happiness.',
-  //     type: 'website',
-  //     url: `https://shipshape.io/blog/author/${encodeURIComponent(author)}/`
-  //   });
-  // }
+  headData: service(),
+
+  async model({ author }) {
+    const posts = await this.store.findAll('post', { reload: true });
+    return posts.filter((post) => {
+      return post.get('author.id') === author;
+    });
+  },
+
+  async afterModel(model) {
+    const author = await model.get('firstObject.author');
+    const authorId = author.id;
+    const authorName = author.name;
+    return setProperties(this.headData, {
+      title: `Posts by ${authorName} - Blog - Ship Shape`,
+      description: `${authorName} has written ${model.length} posts for Ship Shape.`,
+      type: 'website',
+      url: `https://shipshape.io/blog/author/${encodeURIComponent(authorId)}/`
+    });
+  }
 });
