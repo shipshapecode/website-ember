f732e865deb5412b34e81d05f9baf4c9071fddb5
Robert Wagner <rwwagner90@gmail.com>
2018-12-18 14:14:32 -0500

    Try JSON-LD

--- app/routes/ember-consulting.js
+++ app/routes/ember-consulting.js
@@ -9,7 +9,21 @@ export default Route.extend({
           'full scale Ember apps for both large and small clients. We put a huge focus on leveling up your team\'s ' +
           'Ember skills, while also churning out bulletproof features for your Ember app.',
         type: 'website',
-        url: 'https://shipshape.io/ember-consulting/'
+        url: 'https://shipshape.io/ember-consulting/',
+        jsonld: {
+          '@context': 'http://schema.org',
+          '@type': 'Service',
+          'name': 'Ember Consulting',
+          'review': {
+            '@type': 'Review',
+            'reviewBody': 'Ship Shape knows what they’re doing. A thoughtful approach, pristine code and a 200% effort in everything they do, makes recommending them a no-brainer.',
+            'author': {
+              '@type': 'Person',
+              'name': 'James Dixon',
+              'jobTitle': 'Founder of Scout'
+            }
+          }
+        }
       }
     };
   }
