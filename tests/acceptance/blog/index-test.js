31f66961dac2a84c05a775dc8269173b1afb3fbc
Robert Wagner <rwwagner90@gmail.com>
2018-12-30 16:41:18 -0500

    Add some meta tests

--- tests/acceptance/blog/index-test.js
+++ tests/acceptance/blog/index-test.js
@@ -1,45 +1,45 @@
 import { module, test } from 'qunit';
-import { currentURL, visit } from '@ember/test-helpers';
+import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | home', function(hooks) {
+module('Acceptance | blog/index', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting / and checking general meta', async function(assert) {
-    await visit('/');
+  test('visiting /blog and checking general meta', async function(assert) {
+    await visit('/blog');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/blog');
     assert.equal(document.head.querySelector('link[rel="canonical"]').href,
-      'https://shipshape.io/', 'canonical link is correct');
+      'https://shipshape.io/blog/', 'canonical link is correct');
     assert.equal(document.head.querySelector('meta[name="referrer"]').content,
       'unsafe-url', 'referrer is always unsafe-url for maximum links');
   });
 
-  test('visiting / and checking article meta', async function(assert) {
-    await visit('/');
+  test('visiting /blog and checking article meta', async function(assert) {
+    await visit('/blog');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/blog');
     assert.notOk(document.head.querySelector('meta[property="article:published_time"]'),
       'article published_time should be hidden, since we are not on an article');
     assert.notOk(document.head.querySelector('meta[property="article:tag"]'),
       'article tags should be hidden, since we are not on an article');
   });
 
-  test('visiting / and checking opengraph meta', async function(assert) {
-    await visit('/');
+  test('visiting /blog and checking opengraph meta', async function(assert) {
+    await visit('/blog');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/blog');
     assert.equal(document.head.querySelector('meta[property="og:site_name"]').content,
       'Ship Shape', 'og site_name is correct');
     assert.equal(document.head.querySelector('meta[property="og:title"]').content,
-      'Ship Shape - Ember.js Code That Won\'t Sink', 'og title is correct');
+      'Blog - Ship Shape', 'og title is correct');
   });
 
-  test('visiting / and checking twitter meta', async function(assert) {
-    await visit('/');
+  test('visiting /blog and checking twitter meta', async function(assert) {
+    await visit('/blog');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/blog');
     assert.equal(document.head.querySelector('meta[name="twitter:title"]').content,
-      'Ship Shape - Ember.js Code That Won\'t Sink', 'twitter title is correct');
+      'Blog - Ship Shape', 'twitter title is correct');
   });
 });
--- tests/acceptance/ember-consulting-test.js
+++ tests/acceptance/ember-consulting-test.js
@@ -5,9 +5,41 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | ember consulting', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /ember-consulting', async function(assert) {
+  test('visiting /ember-consulting and checking general meta', async function(assert) {
     await visit('/ember-consulting');
 
     assert.equal(currentURL(), '/ember-consulting');
+    assert.equal(document.head.querySelector('link[rel="canonical"]').href,
+      'https://shipshape.io/ember-consulting/', 'canonical link is correct');
+    assert.equal(document.head.querySelector('meta[name="referrer"]').content,
+      'unsafe-url', 'referrer is always unsafe-url for maximum links');
+  });
+
+  test('visiting /ember-consulting and checking article meta', async function(assert) {
+    await visit('/ember-consulting');
+
+    assert.equal(currentURL(), '/ember-consulting');
+    assert.notOk(document.head.querySelector('meta[property="article:published_time"]'),
+      'article published_time should be hidden, since we are not on an article');
+    assert.notOk(document.head.querySelector('meta[property="article:tag"]'),
+      'article tags should be hidden, since we are not on an article');
+  });
+
+  test('visiting /ember-consulting and checking opengraph meta', async function(assert) {
+    await visit('/ember-consulting');
+
+    assert.equal(currentURL(), '/ember-consulting');
+    assert.equal(document.head.querySelector('meta[property="og:site_name"]').content,
+      'Ship Shape', 'og site_name is correct');
+    assert.equal(document.head.querySelector('meta[property="og:title"]').content,
+      'Ember.js Consulting and Training - Ship Shape', 'og title is correct');
+  });
+
+  test('visiting /ember-consulting and checking twitter meta', async function(assert) {
+    await visit('/ember-consulting');
+
+    assert.equal(currentURL(), '/ember-consulting');
+    assert.equal(document.head.querySelector('meta[name="twitter:title"]').content,
+      'Ember.js Consulting and Training - Ship Shape', 'twitter title is correct');
   });
 });
