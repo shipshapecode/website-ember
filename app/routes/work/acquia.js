7b00c5901337e39e9fa3c68818a7217313e0f8f9
Robert Wagner <rwwagner90@gmail.com>
2019-01-23 14:00:04 -0500

    Add basic meta for case study pages

--- app/routes/work/acquia.js
+++ app/routes/work/acquia.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return {
+      metaTags: {
+        title: 'Case Study - Acquia',
+        description: '',
+        type: 'website',
+        url: 'https://shipshape.io/work/acquia/'
+      }
+    };
+  }
 });
--- app/routes/work/brokermate.js
+++ app/routes/work/brokermate.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return {
+      metaTags: {
+        title: 'Case Study - Brokermate',
+        description: '',
+        type: 'website',
+        url: 'https://shipshape.io/work/brokermate/'
+      }
+    };
+  }
 });
