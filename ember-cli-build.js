1f3560750d648dd3dc7ff8efa8d25ef433e3efdf
Robert Wagner <rwwagner90@gmail.com>
2019-02-18 19:36:30 -0500

    Remove tracking and leading

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ const { extname } = require('path');
 const yamlFront = require('yaml-front-matter');
 const walkSync = require('walk-sync');
 
-const SERVICE_WORKER_VERSION = '81';
+const SERVICE_WORKER_VERSION = '82';
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
